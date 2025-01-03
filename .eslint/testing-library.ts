071ac56c377897eb1fb38369f07abfef24b7ac70
Kazuhiro Kumagai <f20aa003@gmail.com>
2025-01-03 19:59:17 +0900

    chore: 設定更新

--- .eslint/testing-library.ts
+++ .eslint/testing-library.ts
@@ -1,14 +1,14 @@
 import type { Linter } from 'eslint';
 
 import { sharedTestFiles } from './shared';
-import { fixupPluginRules } from '@eslint/compat';
 import pluginTestingLibrary from 'eslint-plugin-testing-library';
 
 export const testingLibraryConfig: Linter.Config = {
   name: 'eslint/testing-library',
   files: sharedTestFiles,
-  plugins: { 'testing-library': fixupPluginRules(pluginTestingLibrary) },
+  plugins: { 'testing-library': pluginTestingLibrary },
   rules: {
+    ...pluginTestingLibrary.configs['flat/react'].rules,
     'testing-library/prefer-query-by-disappearance': 'error',
     'testing-library/no-dom-import': 'error',
     'testing-library/no-debugging-utils': 'error',
